79740f0d889c1b775d9bdbce463c1db315a41617
Hajime Yamaguchi <gen.yamaguchi0@gmail.com>
2018-01-06 00:06:11 +0900

    remove unrecognized options: '--enable-module-schnorr'

--- install_lib.sh
+++ install_lib.sh
@@ -2,6 +2,6 @@
 
 pushd secp256k1
 ./autogen.sh
-./configure --enable-module-recovery --enable-experimental --enable-module-ecdh --enable-module-schnorr
+./configure --enable-module-recovery --enable-experimental --enable-module-ecdh
 make && sudo make install
 popd
